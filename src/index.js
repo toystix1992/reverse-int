5147441e036dfe15e82a5c1de8e31c79f9a0b5a2
Parfionov Anton <toystix1992@gmail.com>
2021-01-24 16:35:27 +0300

    init: Add final solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) n*=-1;
+    let str = String(n);
+    let  revNum = str.split('').reverse().reduce((a, b) => a.concat(b))*1;
+    return revNum
 }
